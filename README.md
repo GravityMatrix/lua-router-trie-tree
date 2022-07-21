5b4535c13db40b3a439f16c373d95df20f4a6995
王晨 <wangchen@yunmai365.com>
2022-07-21 15:54:18 +0800

    提交

--- README.md
+++ README.md
@@ -1,2 +1,24 @@
-# lua-router-trie-tree
-基于trie tree路由匹配树
+# 基于trie tree路由匹配树
+
+
+## 匹配模式
+
+
+匹配优先级:  完整 > 参数 > 模糊
+
+提供模糊匹配:
+
+/api/v1/user/**
+
+提供参数匹配:
+
+/api/v1/user/{variable}/order/{variable}
+
+完整匹配:
+
+/api/v1/user/..
+
+
+
+
+
---
+++ trie_tree_test.lua
@@ -0,0 +1,13 @@
+local trie_tree = require "trie_tree"
+
+local tree = trie_tree:new()
+tree:insert("/api/v1/test", "GET")
+tree:insert("/api/v2/**", "GET")
+tree:insert("/api/v2/users/{variable}", "GET")
+tree:insert("/api/v2/users/{variable}/order/{variable}", "GET")
+
+print(tree:match("/api/v2/users/123/order/5555", "GET"))
+
+tree:remove("/api/v2/users/{variable}/order/{variable}", "GET")
+
+print(tree:match("/api/v2/users/123/order/5555", "GET"))
